b19612699880a3a6be2b3288d50964e5e0e6af18
lakmalp <lakmalp@gmail.com>
2022-05-11 10:56:00 +0530

    change in the way configuration are registered

--- src/Providers/BackendServiceProvider.php
+++ src/Providers/BackendServiceProvider.php
@@ -14,7 +14,7 @@ class BackendServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../../config/premialabs.php', 'premialabs');
+        //
     }
 
     /**
