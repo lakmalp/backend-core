5f8b6b8eda3c4873f54f19564dfffb924f320cc7
lakmalp <lakmalp@gmail.com>
2022-05-31 19:57:28 +0530

    added UserRoleRepo/toggle

--- src/Foundation/UserRole/UserRoleController.php
+++ src/Foundation/UserRole/UserRoleController.php
@@ -31,6 +31,7 @@ class UserRoleController extends FndDatabaseController
       // update
       ['{userRole}/prepareEdit', 'GET', 'prepareEdit'],
       ['{userRole}', 'PATCH', 'update'],
+      ['/toggle', 'PATCH', 'toggle'],
 
       // read
       ['{userRole}', 'GET', 'show'],
@@ -47,8 +48,10 @@ class UserRoleController extends FndDatabaseController
     ];
   }
 
-  // public function list(Request $request)
-  // {
-  //   return Utilities::exec($this, 'list', [$request]);
-  // }
+  public function toggle(Request $request)
+  {
+    $user_id = $request->input('user_id');
+    $role_id = $request->input('role_id');
+    return Utilities::exec($this, 'list', [$user_id, $role_id]);
+  }
 }
