e8c08800f7baf3d9d6b99f6c2a124209db6a7a21
lakmalp <lakmalp@gmail.com>
2022-07-02 10:09:50 +0530

    Update UserPermissionRepo.php

--- src/Foundation/UserPermission/UserPermissionRepo.php
+++ src/Foundation/UserPermission/UserPermissionRepo.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Premialabs\Foundation\Permission\gen\Permission;
+use Illuminate\Support\Facades\Auth;
 
 class UserPermissionRepo
 {
